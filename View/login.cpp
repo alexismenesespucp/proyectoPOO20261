ae80688dcd48ba68213b59abe94e8b91aac4dcbe
Alexis Meneses <alexjose70@gmail.com>
2026-04-20 15:48:25 -0500

    agregando nuevo formulario

---
+++ View/login.cpp
@@ -0,0 +1,11 @@
+#include "login.h"
+using namespace ProyectoPoo20261;
+
+int main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	login form;
+	Application::Run(%form);
+	return 0;
+}
