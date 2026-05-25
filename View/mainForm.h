b40f6ea92c217efa30a8d385349e35b3afc3c7e9
Alexis Meneses <alexjose70@gmail.com>
2026-05-25 16:25:44 -0500

    agregando animaciones

--- View/mainForm.h
+++ View/mainForm.h
@@ -18,6 +18,9 @@ namespace ProyectoPoo20261 {
 		mainForm(void)
 		{
 			InitializeComponent();
+			this->DoubleBuffered = true; // Evita el parpadeo en la animación
+			xPosition = 0;
+			timer1->Enabled = true;
 			//
 			//TODO: Add the constructor code here
 			//
@@ -34,12 +37,15 @@ namespace ProyectoPoo20261 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Timer^ timer1;
+	protected:
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		int xPosition;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,7 +54,12 @@ namespace ProyectoPoo20261 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->SuspendLayout();
+
+			this->timer1->Tick += gcnew System::EventHandler(this, &mainForm::timer1_Tick);
+
 			// 
 			// mainForm
 			// 
@@ -64,33 +75,32 @@ namespace ProyectoPoo20261 {
 #pragma endregion
 	private: System::Void mainForm_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
 		Graphics^ g = e->Graphics;
-		
-		SolidBrush^ greenBrush = gcnew SolidBrush(Color::Green);
-		g->FillRectangle(greenBrush, 10, 10, 50, 20);
-		g->DrawEllipse(Pens::Red, 52, 22, 50, 50);
-		
-		g->DrawRectangle(Pens::Black, 95, 65, 50, 20);
+		Pen^ wavePen = gcnew Pen(Color::Blue, 2); // Pen para la onda
+		SolidBrush^ rectBrush = gcnew SolidBrush(Color::Red); // Brocha para el rectángulo
+
+		// Parámetros de la onda
+		double amplitude = 50;
+		double frequency = 0.05; // Cambiar este valor cambia la longitud de onda
+		double offsetY = 200; // Posición vertical de la onda
+
+		// Dibuja la onda cosenoidal
+		for (int x = 0; x < this->ClientSize.Width; ++x) {
+			double y = offsetY + amplitude * Math::Cos(frequency * x);
+			g->DrawRectangle(Pens::Blue, x, y, 1, 1); // Punto para la onda
+		}
+
+		// Dibuja el rectángulo en movimiento siguiendo la onda cosenoidal
+		double yRect = offsetY + amplitude * Math::Cos(frequency * xPosition);
+		g->FillRectangle(rectBrush, xPosition, yRect - 10, 20, 20);
 
 
-		// Dibuja una línea desde (20, 20) hasta (200, 20)
-		//g->DrawLine(Pens::Black, 20, 20, 200, 20);
+	}
 
-		//// Dibuja un rectángulo con posición (30, 40), ancho 100 y alto 50
-		//g->DrawRectangle(Pens::Blue, 30, 40, 100, 50);
-
-		//// Dibuja un círculo usando un rectángulo delimitador
-		//g->DrawEllipse(Pens::Red, 50, 100, 50, 50);
-
-		//Pen^ customPen = gcnew Pen(Color::Green, 3); // Color Verde, grosor 3
-		//g->DrawLine(customPen, 10, 10, 150, 10);
-
-		//array<Point>^ bezierPoints = { Point(50, 150), Point(80, 40), Point(200, 200), Point(250, 50) };
-		//g->DrawBezier(Pens::Purple, bezierPoints[0], bezierPoints[1], bezierPoints[2], bezierPoints[3]);
-
-		SolidBrush^ redBrush = gcnew SolidBrush(Color::Red);
-		//g->FillRectangle(redBrush, 30, 40, 100, 50);
-
-
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		xPosition += 5; // Incrementa la posición X para mover el rectángulo
+		if (xPosition > this->ClientSize.Width)
+			xPosition = 0; // Repetir movimiento
+		this->Invalidate(); // Refresca la pantalla
 	}
 	};
 }
